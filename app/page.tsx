4d79bd77a6b9c8bf21b23e11cf915463b281635e
harshbisht27 <harshbisht180@gmail.com>
2024-06-08 19:18:27 +0530

    project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
    
    <main className="relative bg-black-100 first-line:flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w7xl w-full">
-        <h1 >helloo jee</h1>
+        <Hero/>
       </div>
     </main>
   );
