dc6864fedcbd7273c270cad61792cbbb35d84b56
harshbisht27 <harshbisht180@gmail.com>
2024-06-19 00:39:20 +0530

    Testimonial, Approach, Footer added...some changes needed

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,22 @@ import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/ui/RecentProjects";
 import { navItems } from "@/data/index";
 import Clients from "@/components/ui/Clients";
+import Experience from "@/components/ui/Experience";
+import Approach from "@/components/ui/Approach";
+import Footer from "@/components/ui/Footer";
 export default function Home() {
   return (
    
-   <main className="relative bg-black-100 first-line:flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w7xl w-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero/>
         <Grid/>
         <RecentProjects/>
         <Clients/>
+        <Experience/>
+        <Approach/>
+        <Footer/>
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -107,69 +107,37 @@ export const navItems = [
   export const testimonials = [
     {
       quote:
-        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
       name: "Michael Johnson",
       title: "One of my Collegaue",
     },
     {
       quote:
-        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
       name: "Michael Johnson",
       title: "One of my Collegaue",
     },
     {
       quote:
-        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
       name: "Michael Johnson",
       title: "One of my Collegaue",
     },
     {
       quote:
-        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
       name: "Michael Johnson",
       title: "One of my Collegaue",
     },
     {
       quote:
-        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Harsh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Harsh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Harsh is the ideal partner.",
       name: "Michael Johnson",
       title: "One of my Collegaue",
     },
   ];
   
-  export const companies = [
-    {
-      id: 1,
-      name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 5,
-      name: "docker.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
-    },
-  ];
-  
+ 
   export const workExperience = [
     {
       id: 1,
@@ -182,7 +150,7 @@ export const navItems = [
       id: 2,
       title: "Mobile App Dev - JSM Tech",
       desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
+      className: "md:col-span-2", 
       thumbnail: "/exp2.svg",
     },
     {
@@ -213,5 +181,6 @@ export const navItems = [
     {
       id: 3,
       img: "/link.svg",
+      link: "https://www.linkedin.com/in/harshbisht27/",
     },
   ];
